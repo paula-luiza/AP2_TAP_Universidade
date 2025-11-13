e2f7cf4c278de0f3671b2b6f9cdbb9f8a7c89258
Paula Luiza Rocha de Oliveira <142267093+paula-luiza@users.noreply.github.com>
2025-11-13 18:14:08 -0300

    Add Professor entity with attributes and relationships

---
+++ universidade/src/main/java/br/edu/ibmec/entity/Professor.java
@@ -0,0 +1,36 @@
+package br.edu.ibmec.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Getter
+@Setter
+@Table(name = "professores")
+
+public class Professor {
+
+    @Id
+    private String cpf;
+
+    private String nome;
+
+    @Embedded
+    private Data dataNascimento;
+    
+    @Enumerated(EnumType.STRING)
+    private EstadoCivil estadoCivil;
+
+    @OneToMany
+    private List<Turma> turmas = new ArrayList<Turma>();
+
+
+}
